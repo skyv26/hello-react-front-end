7f52eb8d6f7697d5f691d2a22400acbfa1f6ac04
skyv26 <dev.aakashv@gmail.com>
2023-01-25 17:17:12 +0530

    Add the `useSelector` method for getting state and data

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { DisplayText, Container } from './Greeting.styled';
 
-const Greeting = () => (
-  <Container>
-    <DisplayText>Good Morning 🌅</DisplayText>
-  </Container>
-);
+const Greeting = () => {
+  const state = useSelector((state) => state);
+  const { greeting } = state;
+  return (
+    <Container>
+      <DisplayText>{greeting.length > 0 ? greeting : 'Loading Greeting...'}</DisplayText>
+    </Container>
+  );
+};
 
 export default Greeting;
