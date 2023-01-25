955bc61ae01e59423851996ce25d53533e0d30bf
skyv26 <dev.aakashv@gmail.com>
2023-01-25 12:35:38 +0530

    Add `react-helmet` in index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StyleResetter from './StyleResetter/StyleResetter';
@@ -7,6 +9,13 @@ import StyleResetter from './StyleResetter/StyleResetter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@100;300;400;700&family=Josefin+Sans:wght@100;300;400;700&display=swap" rel="stylesheet" />
+      </Helmet>
+    </HelmetProvider>
     <StyleResetter />
     <App />
   </React.StrictMode>,
